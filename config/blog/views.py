2dcc0c37aa5c9286a8e1f3949ef4caed36546ecc
ahmed sayed <ahmedsayed124680@gmail.com>
2026-01-03 13:26:48 +0200

    END OF HOUR 4 / now deeply understand:
    Slug-based routing
    
    Safe object retrieval
    
    Proper 404 handling
    
    URL design principles
    
    Database vs Python responsibility

--- config/blog/views.py
+++ config/blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('-created')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render (request, 'blog/post_detail.html', {'post':post})
